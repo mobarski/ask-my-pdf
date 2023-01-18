0494cbd444f891016d8ca3fd2ba25cc571827927
Maciej Obarski <mobarski@gmail.com>
2023-01-18 22:42:48 +0100

    0.1.2 fix pdf?

--- pdf.py
+++ pdf.py
@@ -5,9 +5,9 @@ import PyPDF2
 def pdf_to_pages(file):
 	"extract text (pages) from pdf file"
 	pages = []
-	pdf = PyPDF2.PdfReader(file)
-	for p in range(len(pdf.pages)):
-		page = pdf.pages[p]
+	pdf = PyPDF2.PdfFileReader(file)
+	for p in range(pdf.numPages):
+		page = pdf.getPage(p)
 		text = page.extractText()
 		pages += [text]
 	return pages
