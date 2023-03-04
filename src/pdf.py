832c551bc6b6ab93d94ed1803add85636bea3f04
Maciej Obarski <mobarski@gmail.com>
2023-03-05 00:12:41 +0100

    PyPDF2 -> pypdf

--- src/pdf.py
+++ src/pdf.py
@@ -1,13 +1,13 @@
 "PDF adapter"
 
-import PyPDF2
+import pypdf
 
 def pdf_to_pages(file):
 	"extract text (pages) from pdf file"
 	pages = []
-	pdf = PyPDF2.PdfFileReader(file)
-	for p in range(pdf.numPages):
-		page = pdf.getPage(p)
-		text = page.extractText()
+	pdf = pypdf.PdfReader(file)
+	for p in range(len(pdf.pages)):
+		page = pdf.pages[p]
+		text = page.extract_text()
 		pages += [text]
 	return pages
